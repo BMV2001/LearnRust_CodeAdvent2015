d8ec2734f34a802a4c48e6c693d8585da7f3fb62
Orange Cat <2001bmv@gmail.com>
2025-11-10 11:16:33 +0100

    Day 2 commit - Did not manage to finish this one as the calculations seemed right, but gave off wrong answers ;w;

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use std::fs;
 use fs::read_to_string;
+
+
 mod day1;
+mod day2;
 use day1::{getFloor, getFirstBasementValue};
+use day2::calculateSqFeetSum;
 
 fn main() {
-    day1();
+
 
 }
 
+fn day2(){ //Not complete!
+    let input : Result<String, _> = read_to_string("src/inputs/day2Input.txt");
+    let binding : String = input.unwrap();
+    calculateSqFeetSum(binding);
+}
 fn day1(){
     //note to self - learn how to clone/copy rather than borrowing
     let input : Result<String, _> = read_to_string("src/inputs/day1Input.txt");
     let task1 = getFloor(input.unwrap());
     let task2 = getFirstBasementValue(task1.1);
     println!("The answer to task 1 is {}, \nThe answer to task 2 is {}", task1.0, task2);
-
 }
